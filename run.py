23b950651a7dee58d8abecb1242909e8af94abdc
pounde <66019372+pounde@users.noreply.github.com>
2022-05-20 16:48:57 -0400

    infer call working

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ import uvicorn
 # Todo: Update secturity mechanism
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run('main:app', host="127.0.0.1", port=8080, reload=True)
