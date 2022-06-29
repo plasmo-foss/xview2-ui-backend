97440454c6054e0b605142082c2ae1ff50b9be11
pounde <66019372+pounde@users.noreply.github.com>
2022-06-29 09:33:17 -0400

    not_working

--- project/schemas/planet.py
+++ project/schemas/planet.py
@@ -2,6 +2,7 @@ from typing import List, Dict
 from pydantic import BaseModel, Field
 
 
+# Todo: rename me...and my file
 class Planet(BaseModel):
     uid: str = Field(None, example="73a42ed6-901b-4d08-9776-f548620e94ea")
     images: List[Dict] = Field(
@@ -11,12 +12,13 @@ class Planet(BaseModel):
                 "timestamp": "2022-04-07T21:05:38Z",
                 "item_type": "SkySatCollect",
                 "item_id": "20220407_120032_ssc6_u0001",
+                "url": "https://tile{1-3}...."
             },
             {
                 "timestamp": "2022-04-04T11:57:06Z",
                 "item_type": "SkySatCollect",
                 "item_id": "20220404_083931_ssc4_u0001",
+                "url": "Leaflet/Deck.gl compatible url"
             },
         ],
     )
-
