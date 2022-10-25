8a441e7369d8945398202c3f54d11a88ee33c587
pounde <66019372+pounde@users.noreply.github.com>
2022-10-25 11:36:18 -0400

    wip

--- project/backend.py
+++ project/backend.py
@@ -97,11 +97,12 @@ class SkyML(Backend):
         super().__init__()
         self.provider = "Sky"
         self.ACCELERATORS = {"V100": 4}
-        self.LOCAL_MNT = "/home/ubuntu/output"
 
-        self.remote_pre_in_dir = "/home/ubuntu/input/pre"
-        self.remote_post_in_dir = "/home/ubuntu/input/post"
-        self.remote_poly_dir = "/home/ubuntu/input/polys"
+        self.LOCAL_WORK_MNT = "/home/ubuntu/working"
+        self.LOCAL_OUT_MNT = "/home/ubuntu/output"
+        self.WORKING_BUCKET = "xv2-temp-working"  # TODO: This should be a .env variable
+        self.OUTPUT_BUCKET = "xv2-outputs"  # TODO: This should be a .env variable
+
         self.remote_temp_out = "/home/ubuntu/output_temp"
 
     def _make_task(self, command, gpu=False, is_detach=False):
@@ -141,7 +142,7 @@ class SkyML(Backend):
 
     def launch(
         self,
-        s3_bucket: str,
+        s3_bucket: str,  # TODO: Remove this
         job_id: str,
         pre_image_id: str,
         post_image_id: str,
@@ -154,13 +155,69 @@ class SkyML(Backend):
         # set this to be used in task creation
         self.cluster_name = f"xv2-inf-{job_id[-5:]}"
 
+        def _get_imagery(
+            img_provider: str,
+            api_key: str,
+            job_id: str,
+            image_id: str,
+            poly_dict: dict,
+            pre_post: str,
+        ):
+            with sky.Dag() as dag:
+                task = sky.Task(
+                    run=f"aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 316880547378.dkr.ecr.us-east-1.amazonaws.com && docker run --rm -v {self.LOCAL_WORK_MNT}/{job_id}/input/{pre_post}:/output 316880547378.dkr.ecr.us-east-1.amazonaws.com/xv2-inf-backend:latest python backend_runner.py imagery --provider {img_provider} --api_key {api_key} --job_id {job_id} --out_path /output --pre_post {pre_post} --coordinates '{json.dumps(poly_dict)}' --image_id {image_id}",
+                )
+                store = sky.Storage(name=self.WORKING_BUCKET)
+                store.add_store("S3")
+                task.set_storage_mounts({self.LOCAL_WORK_MNT: store})
+
+            sky.spot_launch(
+                dag,
+            )
+
+        def _get_polys():
+            with sky.Dag() as dag:
+                task = sky.Task(
+                    run=f"aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 316880547378.dkr.ecr.us-east-1.amazonaws.com && docker run --rm -v {self.LOCAL_WORK_MNT}/{job_id}/input/polys:/output 316880547378.dkr.ecr.us-east-1.amazonaws.com/xv2-inf-backend:latest python backend_runner.py fetch_polys --job_id {job_id} --coordinates '{json.dumps(poly_dict)}'",
+                )
+                store = sky.Storage(name=self.WORKING_BUCKET)
+                store.add_store("S3")
+                task.set_storage_mounts({self.LOCAL_WORK_MNT: store})
+
+            sky.spot_launch(
+                dag,
+            )
+
+        # get imagery
+        for pre_post in ["pre", "post"]:
+            if pre_post == "pre":
+                img_id = pre_image_id
+            else:
+                img_id = post_image_id
+            _get_imagery(
+                img_provider=img_provider,
+                api_key=img_api_key,
+                job_id=job_id,
+                image_id=img_id,
+                poly_dict=poly_dict,
+                pre_post=pre_post,
+            )
+
+        #get OSM polys
+        inf_xtra_args = []
+        if get_osm:
+            _get_polys()
+            inf_xtra_args.append("--bldg_polys /input/polys/polys.geojson")
+
         try:
             with sky.Dag() as dag:
                 resources = sky.Resources(sky.AWS(), accelerators=self.ACCELERATORS)
                 task = sky.Task(run="echo start", workdir=".").set_resources(resources)
-                store = sky.Storage(name=s3_bucket)
-                store.add_store("S3")
-                task.set_storage_mounts({self.LOCAL_MNT: store})
+                work_store = sky.Storage(name=self.WORKING_BUCKET)
+                out_store = sky.Storage(name=self.OUTPUT_BUCKET)
+                work_store.add_store("S3")
+                out_store.add_store("S3")
+                task.set_storage_mounts({self.LOCAL_WORK_MNT: work_store, self.LOCAL_OUT_MNT: out_store})
 
             sky.launch(
                 dag,
@@ -175,56 +232,28 @@ class SkyML(Backend):
             )
             self._make_task(
                 "docker pull 316880547378.dkr.ecr.us-east-1.amazonaws.com/xv2-inf-engine:latest",
-                is_detach=True,
             )
-            self._make_task(
-                "docker pull 316880547378.dkr.ecr.us-east-1.amazonaws.com/xv2-inf-backend:latest"
-            )
-
-            # get imagery
-            for pre_post in ["pre", "post"]:
-
-                if pre_post == "pre":
-                    img_id = pre_image_id
-                    remote_dir = self.remote_pre_in_dir
-                else:
-                    img_id = post_image_id
-                    remote_dir = self.remote_post_in_dir
-
-                self._make_task(
-                    f"docker run --rm -v {remote_dir}:/output 316880547378.dkr.ecr.us-east-1.amazonaws.com/xv2-inf-backend:latest python backend_runner.py imagery --provider {img_provider} --api_key {img_api_key} --job_id {job_id} --image_id {img_id} --coordinates '{json.dumps(poly_dict)}' --out_path /output --pre_post {pre_post}"
-                )
-
-            # create list for extra args
-            inf_xtra_args = []
-
-            # get OSM polygons
-            if get_osm:
-                self._make_task(
-                    f"docker run --rm -v {self.remote_poly_dir}:/output 316880547378.dkr.ecr.us-east-1.amazonaws.com/xv2-inf-backend:latest python backend_runner.py fetch_polys --job_id {job_id} --coordinates '{json.dumps(poly_dict)}'"
-                )
-                
-                inf_xtra_args.append("--bldg_polys /input/polys/polys.geojson")
 
             # run xv2
+            # TODO: Write to temp out then copy to S3 mount and some files have trouble writing to NFS
             self._make_task(
-                # Todo: currently skips using bldg_polys
-                f"docker run --rm --gpus all --shm-size 56g -v {self.remote_pre_in_dir}:/input/pre -v {self.remote_post_in_dir}:/input/post -v {self.remote_temp_out}:/output -v {self.remote_poly_dir}:/input/polys 316880547378.dkr.ecr.us-east-1.amazonaws.com/xv2-inf-engine:latest --dp_mode {' '.join(inf_xtra_args)}",  # BUG: Bug in inference engine does not produce correct outputs with 4 GPUs unless run in dp_mode. Adding flag as stopgap
+                f"docker run --rm --gpus all --shm-size 56g -v {self.LOCAL_WORK_MNT}/{job_id}/input/pre:/input/pre -v {self.LOCAL_WORK_MNT}/{job_id}/input/post:/input/post -v {self.LOCAL_WORK_MNT}/{job_id}/input/polys:/input/polys -v {self.remote_temp_out}:/output 316880547378.dkr.ecr.us-east-1.amazonaws.com/xv2-inf-engine:latest --dp_mode {' '.join(inf_xtra_args)}",  # BUG: Bug in inference engine does not produce correct outputs with 4 GPUs unless run in dp_mode. Adding flag as stopgap
                 gpu=True,
             )
 
-            # Debug: check checksum before pussing to S3 to check Sky S3 storage
-            self._make_task(f"md5sum {self.remote_temp_out}/mosaics/overlay.tif")
+            # Debug: check checksum before pushing to S3 to check Sky S3 storage
+            # self._make_task(f"md5sum {self.remote_temp_out}/mosaics/overlay.tif")
 
             # move output to S3 mount
             self._make_task(
-                f"mkdir {self.LOCAL_MNT}/{job_id} && sudo cp -r {self.remote_temp_out}/* {self.LOCAL_MNT}/{job_id}"
+                f"mkdir -p {self.LOCAL_OUT_MNT}/{job_id} && sudo cp -r {self.remote_temp_out}/* {self.LOCAL_WORK_MNT}/{job_id}"
             )
 
         except:
             pass
 
         else:
+            # TODO: Check that inference succeeded
             # persist results
             s3 = boto3.resource("s3")
 
@@ -263,7 +292,9 @@ class SkyML(Backend):
 
         # teardown instance
         finally:
-            sky.down(self.cluster_name)
+            # Debug: remove to shutdown
+            # sky.down(self.cluster_name)
+            pass
 
 
 # job polling
